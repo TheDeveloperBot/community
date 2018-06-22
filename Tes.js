927a57e3bc090f523e77fd30e971edf894708b35
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-06-22 12:52:24 +0400

    Create Tes.js

---
+++ Tes.js
@@ -0,0 +1,82 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var config = require("./config.json");
+
+client.login(config.token);
+
+client.on('ready', () => {
+  	client.user.setActivity(config.prefix + " help", { type: 'LISTENING' });
+});
+
+client.on('message', async message => {
+
+	let channel = client.channels.get(config.radioChannelId);
+
+	if(message.author.bot) return;
+	if(message.content.indexOf(config.prefix) !== 0) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if(command === "play") {
+
+		if(config.radioChannelId) {
+
+			const connection = await channel.join();
+
+			const dispatcher = connection.playStream(config.radioStream, {});
+			client.user.setActivity(config.radioName, { type: 'LISTENING' });
+			/* Starting stream message */
+			message.reply("I start broadcasting the radio **" + config.radioName + "** in **" + channel.name + "**.");
+
+		} else {
+
+			if (message.member.voiceChannel) {
+
+				const connection = await message.member.voiceChannel.join();
+
+				const dispatcher = connection.playStream(config.radioStream, {});
+				client.user.setActivity(config.radioName, { type: 'LISTENING' });
+				/* Starting stream message */
+				message.reply("I start broadcasting the radio **" + config.radioName + "** in **" + message.member.voiceChannel.name + "**.");
+					
+			} else {
+
+				/* Error if no voice channel joined) */
+				message.reply("you're not in vc..");
+				
+			}
+
+		}
+	}
+
+	if(command === "stop") { 
+
+		const voiceConnection = client.voiceConnections.find(val => val.channel.guild.id == message.guild.id);
+
+		/* Error (wrong textChannel message) */
+		if (voiceConnection === null) return message.reply("I'm not broadcasting.");
+
+		/* Stopping stream message */
+		message.reply("I stop broadcasting the radio **" + config.radioName + "**.");
+
+		voiceConnection.player.dispatcher.end();
+		voiceConnection.disconnect();
+
+		client.user.setActivity(config.prefix + " help", { type: 'LISTENING' });
+	}
+
+	if(command === "help") {
+		/* Help message (listing commands) */
+		message.reply("I'm broadcasting the radio " + config.radioName + ". The orders I understand are : \n"
+		+ "*" + config.prefix + " play* : starts the broadcast ; \n"
+		+ "*" + config.prefix + " stop* : stop the broadcast.n"
+		+ "We're working to add more radio./__");
+	}
+
+	/*Development function
+	if (command === 'sd') {
+		process.exit(0);
+	}*/
+});
