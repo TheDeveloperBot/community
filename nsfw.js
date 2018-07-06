17dcc38a41b0b7f00a859ad3a81c3abc269d2e27
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-07-06 13:47:07 -0700

    Add files via upload

---
+++ nsfw.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("```All The NSFW Commands:\n 4k\n amateur\n asian\n ecchi\n neko\n ass\n boob\n cosplay\n gif\n hentai\n fuck\n (IN WORK)pornhub\n (IN WORK)porn\n pussy\n snapchat\n milf\n bbw\n dick\n public\n uniform\n\nOther Commands: \n help\n invite```")
+}
